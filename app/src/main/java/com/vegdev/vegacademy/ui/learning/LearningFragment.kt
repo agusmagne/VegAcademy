b84826640d26e6fcaead15fa70841a5fee712af5
agusmagne <agusmagne@gmail.com>
2020-06-28 00:34:45 -0300

    coordinator layout avance

--- app/src/main/java/com/vegdev/vegacademy/ui/learning/LearningFragment.kt
+++ app/src/main/java/com/vegdev/vegacademy/ui/learning/LearningFragment.kt
@@ -1,38 +1,26 @@
 package com.vegdev.vegacademy.ui.learning
 
-import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.vegdev.vegacademy.R
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_learning.*
 
 class LearningFragment : Fragment() {
 
-    private lateinit var learningViewModel: LearningViewModel
     private lateinit var navController: NavController
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        learningViewModel =
-                ViewModelProviders.of(this).get(LearningViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_learning, container, false)
-        learningViewModel.text.observe(this, Observer {
-        })
-
-
-        return root
+        return inflater.inflate(R.layout.fragment_learning, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -49,7 +37,8 @@ class LearningFragment : Fragment() {
 
 
         articles_rv.apply {
-            val articles_categories = resources.obtainTypedArray(R.array.articles_categories_drawables)
+            val articles_categories =
+                resources.obtainTypedArray(R.array.articles_categories_drawables)
             val articles_titles = resources.obtainTypedArray(R.array.articles_categories_titles)
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
             adapter = LearningRvAdapter(navController, articles_categories, articles_titles)
